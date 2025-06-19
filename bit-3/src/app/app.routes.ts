1b6120d1d482665befcab3796d547f6b286501bc
David Carrasco <davidack123456789@gmail.com>
2025-06-18 23:33:35 -0500

    Se crearon mas componentes y realizamos enrutaciones

--- bit-3/src/app/app.routes.ts
+++ bit-3/src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import{Login }from "../app/componentes/paginas/login/login"
+import { Productos } from "../app/componentes/paginas/productos/productos"
+import { PageNotFound } from './componentes/paginas/page-not-found/page-not-found';
+ 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+
+{
+    path: "login",
+    component: Login,
+    title: "Login/Registro"
+
+}
+,
+{
+    path: "productos",
+    component: Productos,
+    title: "Productos/servixios",
+},
+{
+path: "**",
+component: PageNotFound,
+title: "Error 404"
+
+}
+
+
+];
---
+++ bit-3/src/app/componentes/shared/navegacion/navegacion.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navegacion',
+  imports: [],
+  templateUrl: './navegacion.html',
+  styleUrl: './navegacion.css'
+})
+export class Navegacion {
+
+}
